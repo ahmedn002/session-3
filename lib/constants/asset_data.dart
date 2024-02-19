2b4b11fe6b43e8a8f43092c3c99ce956ea3b1442
Goru02 <ahmmednoaman5555@gmail.com>
2024-02-19 11:41:49 +0200

    Initial

---
+++ lib/constants/asset_data.dart
@@ -0,0 +1,3 @@
+class AssetData {
+  static const String imagePath = 'assets/image.png';
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+      ),
+      body: Center(child: Icon(Icons.contact_support_sharp)),
+    );
+  }
+}
