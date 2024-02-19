9c705a3cb726e0a8c116a4eb8096179e23d18b60
Goru02 <ahmmednoaman5555@gmail.com>
2024-02-19 13:28:31 +0200

    final

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:session3/util/validators.dart';
 import 'package:session3/widgets/input_field.dart';
 
 class RegistrationPage extends StatefulWidget {
@@ -9,27 +10,85 @@ class RegistrationPage extends StatefulWidget {
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
-  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            'Enter your data',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Text(
+                  'Enter your data',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                InputField(
+                  controller: _userNameController,
+                  hintText: 'Enter your username',
+                  prefix: Icon(Icons.person),
+                  validator: (String? input) => Validators.checkLengthValidator(input, 7),
+                ),
+                const SizedBox(height: 10),
+                InputField(
+                    controller: _firstNameController,
+                    hintText: 'Enter your first name',
+                    prefix: Icon(Icons.person),
+                    validator: (String? input) => Validators.checkLengthValidator(input, 7)),
+                const SizedBox(height: 10),
+                InputField(
+                    controller: _lastNameController,
+                    hintText: 'Enter your last name',
+                    prefix: Icon(Icons.person),
+                    validator: (String? input) => Validators.checkLengthValidator(input, 7)),
+                const SizedBox(height: 10),
+                InputField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    hintText: 'Enter your email',
+                    prefix: Icon(Icons.alternate_email),
+                    validator: (String? input) => Validators.checkLengthValidator(input, 7)),
+                const SizedBox(height: 10),
+                InputField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    hintText: 'Enter your password',
+                    prefix: Icon(Icons.lock),
+                    validator: (String? input) => Validators.checkLengthValidator(input, 7)),
+                const SizedBox(height: 10),
+                InputField(
+                    controller: _confirmPasswordController,
+                    obscureText: true,
+                    hintText: 'Confirm your password',
+                    prefix: Icon(Icons.lock),
+                    validator: (String? input) => Validators.checkLengthValidator(input, 7)),
+                const SizedBox(height: 20),
+                ElevatedButton(onPressed: _register, child: Text('Register')),
+              ],
             ),
           ),
-          InputField(
-            controller: _nameController,
-            keyboardType: TextInputType.emailAddress,
-            obscureText: true,
-          ),
-        ],
+        ),
       ),
     );
+  }
+
+  void _register() {
+    if (_formKey.currentState?.validate() ?? false) {
+      debugPrint(_userNameController.text);
+    }
   }
 }
--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -4,20 +4,51 @@ class InputField extends StatelessWidget {
   final TextEditingController controller;
   final bool obscureText;
   final TextInputType keyboardType;
-  const InputField({
-    super.key,
-    required this.controller,
-    this.obscureText = false,
-    this.keyboardType = TextInputType.text,
-  });
+  final Widget? prefix;
+  final Widget? suffix;
+  final String hintText;
+  final String? Function(String?) validator;
+
+  const InputField(
+      {super.key,
+      required this.controller,
+      this.obscureText = false,
+      this.keyboardType = TextInputType.text,
+      this.prefix,
+      this.suffix,
+      required this.hintText,
+      required this.validator});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
-      decoration: InputDecoration(),
+      validator: validator,
+      decoration: InputDecoration(
+        focusedBorder: _getBorder(),
+        focusedErrorBorder: _getBorder(color: Colors.red),
+        enabledBorder: _getBorder(),
+        errorBorder: _getBorder(color: Colors.red),
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+        ),
+        prefixIcon: prefix,
+        suffixIcon: suffix,
+      ),
       obscureText: obscureText,
       keyboardType: keyboardType,
+    );
+  }
+
+  InputBorder _getBorder({Color color = Colors.black}) {
+    return OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 2,
+        color: color,
+      ),
+      borderRadius: BorderRadius.circular(100),
     );
   }
 }
